212a0316941be2de9a763c9ee134a4248761d4d6
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2023-04-14 15:57:28 +0100

    Basic state setup

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -6,15 +6,28 @@ import Form from './Components/Form/Form';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: testData
+    };
+  }
+
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
         <Form />
-        <CardList />
+        <CardList profiles={this.state.profiles} />
       </div>
     )
   }
 }
+
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
 
 export default App;
--- github-cards/src/Components/CardList/CardList.js
+++ github-cards/src/Components/CardList/CardList.js
@@ -1,15 +1,10 @@
 import Card from '../Card/Card';
 import './CardList.css';
 
-const CardList = () => (
+const CardList = (props) => (
     <div>
-        {testData.map(profile => <Card {...profile}/>)}
+        {props.profiles.map(profile => <Card {...profile}/>)}
     </div>
 );
 
-const testData = [
-    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-];
 export default CardList;
